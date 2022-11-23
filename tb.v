6b7226cf182e49a1ec35f4f5efc337e6cd1dae4e
Alanirie <113445361+Alanirie@users.noreply.github.com>
2022-11-22 19:40:33 -0500

    Update tb.v

--- tb.v
+++ tb.v
@@ -7,14 +7,16 @@ output [4:0] RD, RS1, RS2;
 output [11:0] IMM;
 integer i;
 
+Top_PC DUT(next, current, reset, clk, RD, RS1, RS2, IMM, out);
 
 initial begin
 	reset <= 1;
-	for(i=0; i <= 20; i = i + 1)
+	for(i=0; i <= 20; i = i + 1) begin
 		clk <= 0;
 		#5
 		clk <= 1;
 		#5;
+	end
 
 end
 
