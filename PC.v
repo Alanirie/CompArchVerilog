e1bd91f56c1c7ced31a1b0182bd4020aa980848f
Alanirie <113445361+Alanirie@users.noreply.github.com>
2022-11-22 19:39:52 -0500

    Update PC.v

--- PC.v
+++ PC.v
@@ -2,12 +2,13 @@ module PC(in, out, reset, clk);
 	input [7:0] in;
 input reset, clk; 
 output reg [7:0] out;
-always @(posedge clk)
+always @(posedge clk or posedge reset)
 begin
-	if(reset)
-		out = 8'h0;
-	else
-		out = in;
-end
+	if(reset) begin
+		out <= 8'd0;
+	end
+	else begin
+		out <= in; // out + 4;
+		end
+	end
 endmodule
-
